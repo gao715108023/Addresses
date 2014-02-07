2f0d0ce3968ac482cea49dcb8a4c616944b5134a
高传俊 <gaochuanjun@gaochuanjundeMacBook-Pro.local>
2014-02-07 16:36:20 +0800

    Initial Commit

---
+++ Addresses/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Addresses
+//
+//  Created by 高传俊 on 14-2-7.
+//  Copyright (c) 2014年 高传俊. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
